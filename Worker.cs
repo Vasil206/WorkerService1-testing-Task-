72c5ce62d8ea3c91126dd20bc899f22e5e0e4298
Vasil206 <shevelenko.vasili@gmail.com>
2024-03-29 22:01:20 +0300

    nats only pushing messages(strings) in one stream("subject".>)

--- Worker.cs
+++ Worker.cs
@@ -2,22 +2,32 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.Diagnostics.Metrics;
 using Microsoft.Extensions.Options;
+using NATS.Client.Core;
+using NATS.Client.JetStream;
+using NATS.Client.JetStream.Models;
 
 namespace WorkerService1
 {
-    public class Worker : BackgroundService
+    public class Worker : BackgroundService//////////////////////////SubjectName, StreamName,"nats://nats_server:4222", e.g. -- getting from config by IOption<T>
     {
+        private const string SubjectName = "worker_metrics";
+        private const string StreamName = "worker_metrics";
+
+        private readonly NatsConnection _nats;
+        private readonly NatsJSContext _js;
+
         private readonly ILogger<Worker> _logger;
+
         private readonly IOptionsMonitor<Data> _dataMonitor;
         private readonly IDisposable? _onDataChange;
         private bool _dataChanged;
 
-        private Dictionary<NameId, CpuRssValue> _measurements;
+        private readonly Dictionary<NameId, CpuRssValue> _measurements;
         private readonly Meter _meter;
 
         private async Task<KeyValuePair<NameId, CpuRssValue>> DoWorkAsync(Process proc)
         {
-            NameId nameId = new NameId(proc.Id, proc.ProcessName);
+            var nameId = new NameId(proc.Id, proc.ProcessName);
 
             double usageCpuTotal;
             try
@@ -27,6 +37,7 @@ namespace WorkerService1
                 long startTime = Environment.TickCount64;
 
                 await Task.Delay(_dataMonitor.CurrentValue.Interval / 2);
+                proc.Refresh();
 
                 if (proc.HasExited) throw new Exception("process has exited");
                 TimeSpan endUsageCpu = proc.TotalProcessorTime;
@@ -39,7 +50,7 @@ namespace WorkerService1
             catch (Win32Exception ex)
             {
                 usageCpuTotal = -ex.NativeErrorCode;
-                _logger.LogWarning($"{nameId.Name} : {nameId.Id} => {ex.Message}    CPU");
+                //_logger.LogWarning($"{nameId.Name} : {nameId.Id} => {ex.Message}    CPU");
             }
             catch when (proc.HasExited)
             {
@@ -60,7 +71,7 @@ namespace WorkerService1
             }
             catch (Win32Exception ex)
             {
-                _logger.LogWarning($"{nameId.Name} : {nameId.Id} => {ex.Message}    RSS");
+                //_logger.LogWarning($"{nameId.Name} : {nameId.Id} => {ex.Message}    RSS");
                 rss = -ex.NativeErrorCode;
             }
             catch (Exception ex)
@@ -74,6 +85,9 @@ namespace WorkerService1
         }
         public Worker(ILogger<Worker> logger, IOptionsMonitor<Data> dataMonitor)
         {
+            _nats = new NatsConnection(NatsOpts.Default with{Url = "nats://nats_server:4222" });
+            _js = new NatsJSContext(_nats);
+
             _dataChanged = false;
             _logger = logger;
 
@@ -96,32 +110,83 @@ namespace WorkerService1
 
             return result;
         }
-        private void ClearMeasurements(ref Dictionary<NameId, CpuRssValue> measurements)
+        private void ClearMeasurements(CancellationToken stoppingToken)
         {
-            var keys = measurements.Keys;
-            foreach (var key in keys)
+            var keys = _measurements.Keys;
+
+            Parallel.ForEach(keys, ClearPlusExitedMessageAsync);
+            return;
+
+            async void ClearPlusExitedMessageAsync(NameId key)
             {
-                if (measurements[key].IsUsed)
-                    measurements.Remove(key);
-                else
-                    measurements[key].IsUsed = true;
+                try
+                {
+                    if (_measurements[key].IsUsed)
+                    {
+                        var ack = await _js.PublishAsync($"{SubjectName}.{key.Name}.{key.Id}", "Exited",
+                            cancellationToken: stoppingToken);
+                        ack.EnsureSuccess();
+
+                        _measurements.Remove(key);
+                    }
+                    else
+                        _measurements[key].IsUsed = true;
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogWarning(ex.Message);
+                }
             }
         }
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
             try
             {
+                //nats
+                INatsJSStream? stream;
+                try
+                {
+                    stream = await _js.GetStreamAsync(StreamName, cancellationToken: stoppingToken);
+                    _logger.LogInformation("Stream got successfully");
+                }
+                catch (NatsJSApiException e) when (e.Error.ErrCode == 10059)
+                {
+                    stream = await _js.CreateStreamAsync(
+                        new StreamConfig(StreamName, new[] { $"{SubjectName}.>" }),
+                        stoppingToken);
+                    _logger.LogInformation("Stream created successfully");
+                }
+                catch (Exception e)
+                {
+                    _logger.LogError(e.Message);
+                    throw;
+                }
+
+                var goodStreamConfig = new StreamConfig(StreamName, new[] { $"{SubjectName}.>" })
+                                            {
+                                                MaxMsgsPerSubject = 1
+                                            };
+                if (stream.Info.Config.Equals(goodStreamConfig))
+                {
+                    stream.Info.Config = goodStreamConfig;
+                    await _js.UpdateStreamAsync(stream.Info.Config, stoppingToken);
+                    _logger.LogInformation("Stream config updated successfully");
+                }
+
+
+
                 //metrics get
                 IEnumerable<Measurement<double>> ObserveCpu() => ObserveValues(val => val.Cpu);
                 _meter.CreateObservableGauge("worker_processes_usage_cpu", ObserveCpu, unit: "%");
-                
+
                 IEnumerable<Measurement<double>> ObserveRss() => ObserveValues(val => val.Rss);
                 _meter.CreateObservableGauge("worker_processes_usage_rss", ObserveRss, unit: "mb");
 
 
-                string[] processNames = _dataMonitor.CurrentValue.ProcessNames;
-                PeriodicTimer timer = new PeriodicTimer(TimeSpan.FromMilliseconds(_dataMonitor.CurrentValue.Interval));
+
                 //set
+                string[] processNames = _dataMonitor.CurrentValue.ProcessNames;
+                var timer = new PeriodicTimer(TimeSpan.FromMilliseconds(_dataMonitor.CurrentValue.Interval));
                 while (await timer.WaitForNextTickAsync(stoppingToken))
                 {
                     //going from names to Process
@@ -136,9 +201,9 @@ namespace WorkerService1
                         }
                     }
 
+
                     //making the array of async tasks with calculating of CPU usage;
                     var metricsLoop = new Task<KeyValuePair<NameId, CpuRssValue>>[processes.Length][];
-
                     for (int i = 0; i < metricsLoop.Length; i++)
                     {
                         Array.Resize(ref metricsLoop[i], processes[i].Length);
@@ -149,11 +214,13 @@ namespace WorkerService1
                         }
                     }
 
-                    ClearMeasurements(ref _measurements);
+                    ClearMeasurements(stoppingToken);
+
 
                     //wait while works
                     foreach (Task[] useCpu in metricsLoop)
-                        Task.WaitAll(useCpu);
+                        Task.WaitAll(useCpu, stoppingToken);
+
 
                     //add to measurements
                     foreach (var results in metricsLoop)
@@ -169,19 +236,45 @@ namespace WorkerService1
                                 break;
                             }
                         }
-                        if(!isAdded)
-                            _measurements.Add(result.Result.Key,result.Result.Value);
+
+                        if (!isAdded)
+                            _measurements.Add(result.Result.Key, result.Result.Value);
                     }
+
+
+                    //upload to nats
+                    Parallel.ForEach(_measurements, UploadToNatsAsync);
+
 
                     //on data change
                     if (_dataChanged)
                     {
-                        _dataChanged=false;
+                        _dataChanged = false;
                         processNames = _dataMonitor.CurrentValue.ProcessNames;
                         timer.Dispose();
                         timer = new(TimeSpan.FromMilliseconds(_dataMonitor.CurrentValue.Interval));
                     }
+
+                    continue;
+
+                    async void UploadToNatsAsync(KeyValuePair<NameId, CpuRssValue> measurement)
+                    {
+                        try
+                        {
+                            var ack = await _js.PublishAsync(
+                                $"{SubjectName}.{measurement.Key.Name}.{measurement.Key.Id}",
+                                $"cpu: {measurement.Value.Cpu} || rss: {measurement.Value.Rss}",
+                                cancellationToken: stoppingToken);
+
+                            ack.EnsureSuccess();
+                        }
+                        catch (Exception ex)
+                        {
+                            _logger.LogWarning(ex.Message);
+                        }
+                    }
                 }
+
             }
             catch (Exception ex)
             {
@@ -191,6 +284,7 @@ namespace WorkerService1
             {
                 _meter.Dispose();
                 _onDataChange?.Dispose();
+                await _nats.DisposeAsync();
             }
         }
 
